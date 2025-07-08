398ceba5af7c17400665413703252f002b230ec2
youjeong Jeong <wjddbwjd1205@naver.com>
2025-07-09 03:11:50 +0900

    emotion의 css 사용 테스트

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,9 +1,21 @@
+import { css } from '@emotion/react';
+
 const Button = () => {
-  return (
-    <button className='bg-blue-500 text-white font-bold py-2 px-4 rounded'>
-      Click Me
-    </button>
-  );
+  return <button css={buttonStyle}>Click Me</button>;
 };
 
 export default Button;
+
+const buttonStyle = css`
+  background-color: #3b82f6; /* bg-blue-500 */
+  color: white;
+  font-weight: bold;
+  padding: 0.5rem 1rem; /* py-2 px-4 */
+  border-radius: 0.375rem; /* rounded */
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #2563eb; /* hover:bg-blue-600 */
+  }
+`;
